81d663f90189162e001345de6e3ed622fb8c0012
the-evillevi <levi.carbellido@outlook.com>
2023-09-13 08:05:52 -0600

    feat: add section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-import Home from "@/sections/Home";
 import About from "@/sections/About";
-import Projects from "@/sections/Projects";
-import Skills from "@/sections/Skills";
-import Experience from "@/sections/Experience";
 import Contact from "@/sections/Contact";
+import Experience from "@/sections/Experience";
+import Home from "@/sections/Home";
+import Projects from "@/sections/Projects";
+import SectionDivider from "@/components/SectionDivider";
+import Skills from "@/sections/Skills";
 
 export default function Main() {
   return (
     <main className="flex flex-col items-center px-4">
       <Home />
+      <SectionDivider />
       <About />
+      <SectionDivider />
       <Projects />
+      <SectionDivider />
       <Skills />
+      <SectionDivider />
       <Experience />
+      <SectionDivider />
       <Contact />
     </main>
   );
---
+++ components/SectionDivider.tsx
@@ -0,0 +1,5 @@
+export default function SectionDivider() {
+  return (
+    <div className="my-24 hidden h-16 w-1 rounded-full bg-gray-200 sm:block"></div>
+  );
+}
